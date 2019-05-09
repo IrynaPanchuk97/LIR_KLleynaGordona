41b96db07ab22cb6a23cf6c219cbd4924ed89bf4
IrynaPanchuk97 <rusa1234527@gmail.com>
2019-05-09 15:40:31 +0300

    Add project files.

---
+++ DirectKleinGordonInteriorNeumann/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectKleinGordonInteriorNeumann
+{
+    class Program
+    {
+        static double[] yStar = { -2, 4 };
+        const int k = 2;
+        static double x1(double t)
+        {
+            return 3*Math.Cos(t);
+        }
+        static double x2(double t)
+        {
+            return Math.Sin(t)+ Math.Cos(t);
+        }
+        static double x1d(double t)
+        {
+            return -3*Math.Sin(t);
+        }
+        static double x2d(double t)
+        {
+            return Math.Cos(t) - Math.Sin(t);
+        }
+        static double x1dd(double t)
+        {
+            return -3*Math.Cos(t);
+        }
+        static double x2dd(double t)
+        {
+            return -Math.Sin(t)-Math.Cos(t);
+        }
+        static double diffModule(double t, double tau)
+        {
+            return Math.Sqrt((x1(t) - x1(tau)) * (x1(t) - x1(tau)) + (x2(t) - x2(tau)) * (x2(t) - x2(tau)));
+        }
+        static double derModule(double t)
+        {
+            return Math.Sqrt(x1d(t) * x1d(t) + x2d(t) * x2d(t));
+        }
+
+        static double h(double t, double tau)
+        {
+            double numerator = ((x1(tau) - x1(t)) * x2d(tau) - (x2(tau) - x2(t)) * x1d(tau));
+            double denominator = diffModule(t, tau);
+            return numerator / denominator;
+        }
+        static double L(double t, double tau)
+        {
+            return -2 * k * alglib.besselk1(k * diffModule(t, tau)) * h(t, tau);
+        }
+        static double L1(double t, double tau)
+        {
+            if (t != tau)
+            {
+                return -k * alglib.besseli1(k * diffModule(t, tau)) * h(t, tau);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static double L2(double t, double tau)
+        {
+            if (t != tau)
+            {
+                return L(t, tau) - L1(t, tau) * Math.Log(4 * Math.Sin((t - tau) / 2) * Math.Sin((t - tau) / 2));
+            }
+            else
+            {
+                double numerator = -(x1d(t) * x2dd(t) - x2d(t) * x1dd(t));
+                double denominator = derModule(t) * derModule(t);
+                return numerator / denominator;
+            }
+        }
+        static double D(double t, double tau)
+        {
+            return 2*alglib.besselk0(k * diffModule(t, tau)) * derModule(tau);
+        }
+
+        static double D1(double t, double tau)
+        {
+            if (t != tau)
+            {
+                return -alglib.besseli0(k * diffModule(t, tau)) * derModule(tau) ;
+            }
+            else
+            {
+                return -derModule(t);
+            }
+        }
+
+        static double D2(double t, double tau)
+        {
+            if (t != tau)
+            {
+                return D(t, tau) - D1(t, tau) * Math.Log(4 * Math.Sin((t - tau) / 2) * Math.Sin((t - tau) / 2));
+            }
+            else
+            {
+                return 2*derModule(t) * (Math.Log(2 / (k * derModule(t)))  - EulerConst);
+            }
+        }
+        static double R(int j, double t, int n, double[] ti)
+        {
+            double sum = 0;
+            for (int m = 1; m <= n - 1; m++)
+            {
+                sum = sum + Math.Cos(m * (t - ti[j])) / m;
+            }
+            sum = sum + Math.Cos(n * (t - ti[j])) / (2 * n);
+            return -sum / n;
+        }
+        static double exact(double[] x)
+        {
+            return alglib.besselk0(k * module(x, yStar)) / (2 * Math.PI);
+        }
+        static double module(double[] x, double[] y)
+        {
+            return Math.Sqrt((x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]));
+        }
+        static double g(double t)
+        {
+            return -k * alglib.besselk1(k * module(new double[] { x1(t), x2(t) }, yStar)) * ((x1(t) - yStar[0]) * x2d(t) - (x2(t) - yStar[1]) * x1d(t)) / (2 * Math.PI * derModule(t) * module(new double[] { x1(t), x2(t) }, yStar));
+        }
+
+        static double G(int i,double [] ti,int n)
+        {
+            double sum = 0;
+            for (int j=0;j<2*n;j++)
+            {
+                sum = sum + g(ti[j]) * (D1(ti[i], ti[j]) * R(j, ti[i], n, ti) + D2(ti[i], ti[j]) / (2 * n));
+            }
+            return sum;
+        }
+        static double tildaKernel(double[] x, double tau)
+        {
+            double numerator = -k*alglib.besselk1(k* module(x, new double[] { x1(tau), x2(tau) })) *((x1(tau) - x[0]) * x2d(tau) - (x2(tau) - x[1]) * x1d(tau));
+            double denominator = module(x, new double[] { x1(tau), x2(tau) }) ;
+            return numerator / denominator;
+        }
+        static double numericalSolution(double[] x, int n, double[] density, double[] ti)
+        {
+            double Un = 0;
+            for (int i = 0; i < 2 * n; i++)
+            {
+                Un = Un + g(ti[i]) *alglib.besselk0(k*module(x, new double[] { x1(ti[i]), x2(ti[i]) })) * derModule(ti[i]);
+            }
+            for (int i = 0; i < 2 * n; i++)
+            {
+                Un = Un - density[i] * tildaKernel(x, ti[i]);
+            }
+            Un = Un / (2 * n);
+            return Un;
+        }
+        const double EulerConst = 0.5772156649;
+        static void Main(string[] args)
+        {
+            double[] testPoint = { 0,0};
+            int n = 2;
+            for (int k = 1; k <= 7; k++)
+            {
+                double[] ti = new double[2 * n];
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    ti[i] = Math.PI * i / n;
+                }
+
+                double[,] Matrix = new double[2 * n, 2 * n];
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    for (int j = 0; j < 2 * n; j++)
+                    {
+                        Matrix[i, j] = R(j, ti[i], n, ti) * L1(ti[i], ti[j]) + L2(ti[i], ti[j]) / (2 * n);
+                        if (i == j)
+                        {
+                            Matrix[i, j] = Matrix[i, j] + 1;
+                        }
+                    }
+                }
+
+                double[] rightPart = new double[2 * n];
+                for (int i = 0; i < 2 * n; i++)
+                {
+                    rightPart[i] = G(i,ti,n);
+                }
+
+                librarygauss executor = new librarygauss();
+                var density = executor.gauss(Matrix, rightPart);
+                for (int i = 0; i < density.Length; i++)
+                {
+                    //Console.WriteLine(density[i]);
+                }
+                Console.WriteLine("n = " + n + "    error = " + Math.Abs(numericalSolution(testPoint, n, density, ti) - exact(testPoint)));
+                n = n * 2;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DirectKleinGordonInteriorNeumann/librarygauss.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectKleinGordonInteriorNeumann
+{
+    public class librarygauss
+    {
+        public delegate double func(double x);
+        public delegate double func1(double x, double z);
+        public double square;
+        private int No;
+        private int N;
+
+        public double IntegalTrap(double a, double b, int N, func f)
+        {
+            double h = (b - a) / N, s = 0, TEMP = a;
+            double[] x = new double[N + 1];
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = TEMP;
+                if ((x[i] == a) || (x[i] == b))
+                {
+                    s = s + 1 / 2.0 * f(x[i]);
+                }
+                else
+                {
+                    s = s + f(x[i]);
+                }
+                TEMP = TEMP + h;
+            }
+            return (h * s);
+        }
+
+        public double[] gauss(double[,] matr, double[] b)
+        {
+            int N = b.Length;
+            double[] x = new double[N];
+            double R;
+            try
+            {
+
+                for (int q = 0; q < N; q++)
+                {
+                    R = 1 / matr[q, q];
+                    matr[q, q] = 1;
+                    for (int j = q + 1; j < N; j++)
+                        matr[q, j] *= R;
+                    b[q] *= R;
+                    for (int k = q + 1; k < N; k++)
+                    {
+                        R = matr[k, q];
+                        matr[k, q] = 0;
+                        for (int j = q + 1; j < N; j++)
+                            matr[k, j] = matr[k, j] - matr[q, j] * R;
+                        b[k] = b[k] - b[q] * R;
+                    }
+                }
+            }
+            catch (DivideByZeroException)
+            {
+
+                return x;
+            }
+
+            for (int q = N - 1; q >= 0; q--)
+            {
+                R = b[q];
+                for (int j = q + 1; j < N; j++)
+                    R -= matr[q, j] * x[j];
+                x[q] = R;
+            }
+            return x;
+        }
+
+    }
+}
